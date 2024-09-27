c669447666bdb98b1acda408cd4410bf249a711e
Semenyaka Sergey <Serh.semka@gmail.com>
2024-09-27 11:47:57 +0300

    Faq section implemented

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.group-header');
+
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // Toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // Toggle visibility of body
+        groupBody.classList.toggle('open');
+
+        // Close other open FAQ bodies
+        const otherGroups = faqContainer.querySelectorAll('.content-group');
+
+        otherGroups.forEach((otherGroup) => {
+            if (otherGroup !== group) {
+                const otherGroupBody = otherGroup.querySelector('.group-body');
+                const otherIcon = otherGroup.querySelector('.group-header i');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+        });
+    });
+});
 
